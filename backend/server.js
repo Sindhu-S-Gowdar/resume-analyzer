c2df46159893c29d6677f805c5e5119c2aff6278
Sindhu S Gowdar <sindhusgowdar@Sindhus-MacBook-Air.local>
2026-07-08 21:43:17 +0530

    Add Express server with Gemini API integration

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.post('/analyze', async(req, res)=>{
     try {
         const{ resume, jobDescription } = req.body;
 
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AQ.Ab8RN6J0byokWpOp9CN5fkydfvLKjDUYVs39uwhyHZ4nLep2_Q`, {
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=real-key`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
